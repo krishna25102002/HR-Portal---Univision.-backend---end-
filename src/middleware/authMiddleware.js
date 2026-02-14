6875da073be56a85d0cc2d0c34df85e8c22fc66d
krishna25102002 <tripdaries01@gmail.com>
2026-02-14 13:36:26 +0530

    ..removedunwanterfiles

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -28,23 +28,3 @@ export const protect = async (req, res, next) => {
     res.status(401).json({ message: "Invalid token" });
   }
 };
-
-// import jwt from 'jsonwebtoken';
-
-// export const authMiddleware = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader) {
-//     return res.status(401).json({ message: 'Missing token' });
-//   }
-
-//   const token = authHeader.split(' ')[1];
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded; // 🔥 THIS IS IMPORTANT
-//     next();
-//   } catch (err) {
-//     return res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
--- src/utils/sendMailGraph.js
+++ src/utils/sendMailGraph.js
@@ -41,35 +41,3 @@ export const sendMailViaGraph = async ({
     }
   );
 };
-
-// import axios from "axios";
-// import { getGraphAccessToken } from "./msGraphAuth.js";
-
-// export const sendMailViaGraph = async ({ to, subject, html }) => {
-//   const token = await getGraphAccessToken();
-
-//   const url = `https://graph.microsoft.com/v1.0/users/${process.env.MS_MAIL_SENDER}/sendMail`;
-
-//   const payload = {
-//     message: {
-//       subject,
-//       body: {
-//         contentType: "HTML",
-//         content: html,
-//       },
-//       toRecipients: [
-//         {
-//           emailAddress: { address: to },
-//         },
-//       ],
-//     },
-//     saveToSentItems: true,
-//   };
-
-//   await axios.post(url, payload, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       "Content-Type": "application/json",
-//     },
-//   });
-// };
